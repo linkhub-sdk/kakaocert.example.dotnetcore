9dacb440a904f043d4329d5e38753261f0b815be
JohnJeong123 <frenchofkiss@gmail.com>
2020-09-02 10:38:16 +0900

    added ESign Service - appUseYN

--- KakaocertExample/Controllers/KakaocertController.cs
+++ KakaocertExample/Controllers/KakaocertController.cs
@@ -22,11 +22,14 @@ namespace KakaocertExample.Controllers
         public IActionResult RequestESign()
         {
             /**
-            * 간편 전자서명을 요청합니다.
+            * 전자서명을 요청합니다.
             */
 
             // Kakaocert 이용기관코드, Kakaocert 파트너 사이트에서 확인
             string clientCode = "020040000001";
+
+            // AppToApp 인증여부, true-AppToApp 방식, false-TalkMessage 방식
+            bool isAppUseYN = false;
 
             RequestESign requestObj = new RequestESign();
 
@@ -37,10 +40,10 @@ namespace KakaocertExample.Controllers
             requestObj.Expires_in = 60;
 
             // 수신자 생년월일, 형식 : YYYYMMDD
-            requestObj.ReceiverBirthDay = "19700101";
+            requestObj.ReceiverBirthDay = "19800101";
 
             // 수신자 휴대폰번호
-            requestObj.ReceiverHP = "01012341234";
+            requestObj.ReceiverHP = "010111222";
 
             // 수신자 성명
             requestObj.ReceiverName = "테스트";
@@ -48,9 +51,11 @@ namespace KakaocertExample.Controllers
             // 별칭코드, 이용기관이 생성한 별칭코드 (파트너 사이트에서 확인가능)
             // 카카오톡 인증메시지 중 "요청기관" 항목에 표시
             // 별칭코드 미 기재시 이용기관의 이용기관명이 "요청기관" 항목에 표시
+            // AppToApp 인증시 동작하지 않음.
             requestObj.SubClientID = "";
 
             // 인증요청 메시지 부가내용, 카카오톡 인증메시지 중 상단에 표시
+            // AppToApp 인증시 동작하지 않음.
             requestObj.TMSMessage = "TMSMessage0423";
 
             // 인증요청 메시지 제목, 카카오톡 인증메시지 중 "요청구분" 항목에 표시
@@ -75,8 +80,8 @@ namespace KakaocertExample.Controllers
 
             try
             {
-                var result = _kakaocertService.requestESign(clientCode, requestObj);
-                return View("ReceiptID", result);
+                var result = _kakaocertService.requestESign(clientCode, requestObj, isAppUseYN);
+                return View("ResponseESign", result);
             }
             catch (KakaocertException ke)
             {
@@ -87,18 +92,22 @@ namespace KakaocertExample.Controllers
         public IActionResult GetESignResult()
         {
             /**
-            * 간편 전자서명 결과정보를 확인합니다.
+            * 전자서명 결과정보를 확인합니다.
             */
 
             // Kakaocert 이용기관코드, Kakaocert 파트너 사이트에서 확인
             string clientCode = "020040000001";
 
             // 요청시 반환받은 접수아이디
-            string receiptId = "020050710183000001";
+            string receiptId = "020090210193200001";
+
+            // AppToApp 인증시 앱스킴 success로 반환되는 서명값 기재.
+            // Talk Message  인증시 null 처리.
+            string signature = null;
 
             try
             {
-                var resultObj = _kakaocertService.GetESignResult(clientCode, receiptId);
+                var resultObj = _kakaocertService.GetESignResult(clientCode, receiptId, signature);
                 return View("GetESignResult", resultObj);
             }
             catch (KakaocertException ke)
@@ -107,6 +116,8 @@ namespace KakaocertExample.Controllers
             }
 
         }
+
+
 
         public IActionResult RequestVerifyAuth()
         {
